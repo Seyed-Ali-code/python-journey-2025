f1de0e6270ddb0d24943d133ba2f639e89ac80ef
Seyed Ali <alihomayouni96@gmail.com>
2025-08-06 21:19:07 +0430

    feat: add area_circle with docstring

---
+++ m6_area.py
@@ -0,0 +1,12 @@
+# def sum(a,b):
+#     print(f"{a}+{b} = {a+b}")
+
+
+
+# sum(12,3)
+def area_circle(r):
+    """Return area of a circle given its radius."""
+
+    return r * r * 3.14
+sh = float(input("Shoae : "))
+print("masahat = ",area_circle(sh))
